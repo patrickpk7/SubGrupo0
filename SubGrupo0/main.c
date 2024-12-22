b8a7b3e8f22b16d5d66378228655c4a22ca80649
Patrick Chagas <pkoutlet2020@gmail.com>
2024-12-22 12:41:30 -0300

    Adicionada conversao de temperatura

--- SubGrupo0/main.c
+++ SubGrupo0/main.c
@@ -1,2 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "Embarcatechsub0.h"
+
+int main(){
+    int opcao;
+    do{
+        printf("Escolha uma opcao:\n");
+        printf("1 - Converter de Celsius para Kelvin\n");
+        printf("2 - Converter de Kelvin para Celsius\n");
+        printf("3 - Converter de Celsius para Fahrenheit\n");
+        printf("4 - Converter de Fahrenheit para Celsius\n");
+        printf("5 - Converter de Kelvin para Fahrenheit\n");
+        printf("6 - Converter de Fahrenheit para Kelvin\n");
+        printf("0 - Sair\n");
+        scanf("%d", &opcao);
+        switch(opcao){
+            case 1:
+                temperaturaCelsiusKelvin();
+                break;
+            case 2:
+                temperaturaKelvinCelsius();
+                break;
+            case 3:
+                temperaturaCelsiusFahrenheit();
+                break;
+            case 4:
+                temperaturaFahrenheitCelsius();
+                break;
+            case 5:
+                temperaturaKelvinFahrenheit();
+                break;
+            case 6:
+                temperaturaFahrenheitKelvin();
+                break;
+            case 0:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opcao invalida!\n");
+        }
+    }while(opcao != 0);
+    return 0;
+}
