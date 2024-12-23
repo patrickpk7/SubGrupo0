2dde7445a0d9561139d78d3f3cb375b9c43e586b
Priscila S. <prisuzart@gmail.com>
2024-12-22 22:08:03 -0300

    Inclui biblioteca e cria protótipo das funções

--- Volume.c
+++ Volume.c
@@ -1,2 +1,33 @@
 #include <stdio.h>
 
+// Protótipo das funções
+void convert_volume(float volume, char from_unit, char to_unit);
+
+int main() {
+    
+    system("chcp 65001>nul");
+    
+    float volume;
+    char from_unit, to_unit;
+
+    printf("=== Conversor de Unidades de Volume ===\n");
+    printf("Unidades disponíveis:\n");
+    printf("L - Litros\n");
+    printf("M - Mililitros\n");
+    printf("C - Metros cúbicos\n");
+
+    // Entrada do volume e das unidades
+    printf("\nDigite o volume a ser convertido: ");
+    scanf("%f", &volume);
+
+    printf("Digite a unidade de origem (L/M/C): ");
+    scanf(" %c", &from_unit);
+
+    printf("Digite a unidade de destino (L/M/C): ");
+    scanf(" %c", &to_unit);
+
+    // Chamada da função de conversão
+    convert_volume(volume, from_unit, to_unit);
+
+    return 0;
+}
